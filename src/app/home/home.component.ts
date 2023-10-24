4928e85879afbf403147a2a1d38177e3039d592e
Marcelo Vergara <marcelorv@gmail.com>
2023-10-24 07:12:44 -0300

    Real observable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription, interval } from 'rxjs'
+import { Observable, Subscription, interval } from 'rxjs'
 
 @Component({
   selector: 'app-home',
@@ -14,10 +14,27 @@ export class HomeComponent implements OnInit, OnDestroy{
   constructor() { }
 
   ngOnInit() {
-    this.intervalSubscribe =  interval(1000).subscribe(count => {
-      console.log(count)
+    // First subscription
+    // this.intervalSubscribe =  interval(1000).subscribe(count => {
+    //   console.log(count)
+    // })
+
+    // Second subscription
+    // Real observable
+    const custmoIntervalObs = Observable.create((observer) => {
+      let count = 0
+      setInterval(()=> {
+        // Next to emmit a new value
+        observer.next(count)
+        count++
+        // Error to throw an error
+        // Complete to know when  you are done
+      },1000)
     })
+
+    this.intervalSubscribe = custmoIntervalObs.subscribe(data => console.log(data))
   }
+
 
   ngOnDestroy(): void {
     this.intervalSubscribe.unsubscribe()
